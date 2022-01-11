34d95f5624093f3ea2c4a7a8703f64ec70e240f0
RuslanPanasuk <panasuk.eetp@gmail.com>
2022-01-11 11:09:50 +0200

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 
-module.exports = function towelSort (matrix) {
-  
-  return [];
+module.exports = function towelSort (matrix = []) {
+  let result = [];
+
+  matrix.forEach((item, i) => {
+    if (i % 2 !== 0) {
+      result.push(...(item.reverse()));
+    } else {
+      result.push(...(item));
+    }
+  });
+
+  return result;
 }
